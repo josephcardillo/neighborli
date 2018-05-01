7297e2800f916b7eaeaba0e7baefc4d8c3bd9b47
Noah Callahan <noahc1991@gmail.com>
2018-05-01 13:46:25 -0400

    feedback and transaction validations

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,3 +1,7 @@
 class Feedback < ApplicationRecord
     belongs_to :user
+
+    validates :rating, numericality: { less_than_or_equal_to: 5,  only_integer: true }
+    
+    validates :comment, length: {in: 0..280}
 end

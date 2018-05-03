4579b62fced30a4e752f08ee7a577c988f3023ab
David Elster <davidandtheweb@gmail.com>
2018-05-03 14:58:55 -0400

    Change root route to profile for logged in users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  root :to => "homepage#index"
   devise_for :users
   resources :feedbacks
   resources :transactions
-  resources :users, :only => [:index, :show] 
+  resources :users, :only => [:index, :show]
+
+  authenticated :user do
+    root 'users#index', as: :authenticated_root
+  end
+
+  root "homepage#index"
 end
 

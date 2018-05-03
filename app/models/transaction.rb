8771f547564924e9817598a5e6fa864589b50ead
David Elster <davidandtheweb@gmail.com>
2018-05-03 17:26:44 -0400

    Current Progress

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,17 +1,16 @@
 class Transaction < ApplicationRecord
-    belongs_to :borrower, optional: true, class_name:"User"
-    belongs_to :lender, optional: true, class_name: "User"
-    mount_uploader :avatar, AvatarUploader
+  belongs_to :borrower, optional: true, class_name:"User"
+  belongs_to :lender, optional: true, class_name: "User"
+  mount_uploader :avatar, AvatarUploader
 
-    validates :status, presence: true, length: {in: 4..9}
-    validates :expires_at, presence: true
-    validates :starts_at, presence: true
-    validates :action, presence: true, length: {in: 4..6}
-    validates :category, presence: true, length: {in: 3..12}
-    validates :title, presence: true, length: {in: 3..140}
-    validates :body, presence: true, length: {in: 5..280}
-    validates :item_location, presence: true
-    validates :loan_duration, presence: true
-    validates :day_rate, presence: true
+  validates :status, presence: true, length: {in: 4..9}
+  validates :expires_at, presence: true
+  validates :starts_at, presence: true
+  validates :action, presence: true, length: {in: 4..6}
+  validates :category, presence: true, length: {in: 3..12}
+  validates :title, presence: true, length: {in: 3..140}
+  validates :body, presence: true, length: {in: 5..280}
+  validates :item_location, presence: true
+  validates :loan_duration, presence: true
+  validates :day_rate, presence: true
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users, :only => [:index, :show]
 
   authenticated :user do
-    root 'users#index', as: :authenticated_root
+    root 'homepage#dashboard', as: :authenticated_root
   end
 
   root "homepage#index"

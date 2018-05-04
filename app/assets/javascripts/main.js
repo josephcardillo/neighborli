ede44a52328e68ecd2755d987b562fbfa4dba65d
David Elster <davidandtheweb@gmail.com>
2018-05-04 14:57:26 -0400

    Current Progress

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,5 +2,9 @@ $(document).on("turbolinks:load", () => {
   // Fix labels overlapping prefilled text inputs.
   M.updateTextFields();
   // Initialize our FAB.
-  $(".fixed-action-btn").floatingActionButton({direction: 'left'});
+  $(".fixed-action-btn").floatingActionButton({ direction: "left" });
+  // Initialize DatePickers.
+  $(".datepicker input").datepicker();
+  // Initialize our Selects.
+  $(".input-field select").formSelect();
 });

acb85463e609d578ff4670f738cd999e9d0494a6
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-05-04 12:52:14 -0400

    progress commit

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,6 +2,15 @@ class Feedback < ApplicationRecord
     belongs_to :user
 
     validates :rating, numericality: { less_than_or_equal_to: 5,  only_integer: true }
-    
+
     validates :comment, length: {in: 0..280}
+
+    # def feedback_user_id
+    #   set_transaction
+    #   if current_user.id == @lender.id
+    #     @feedback_id = @borrower.id
+    #   elsif current_user.id == @borrower.id
+    #     @feedback_id = @lender.id
+    #   end
+    # end
 end

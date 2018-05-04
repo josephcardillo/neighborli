7bd408f0fd2375389b0b6c5566d495f144975ccc
David Elster <davidandtheweb@gmail.com>
2018-05-03 22:01:21 -0400

    Add: Magic Comment, Update: Syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
   resources :feedbacks
   resources :transactions
-  resources :users, :only => [:index, :show]
+  resources :users, only: %i[index show]
 
   authenticated :user do
     root 'homepage#dashboard', as: :authenticated_root
   end
 
-  root "homepage#index"
+  root 'homepage#index'
 end
-

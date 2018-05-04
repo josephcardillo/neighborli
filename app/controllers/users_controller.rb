fb2c521d3744acc65511db4f25dd0bd8ee432f20
David Elster <davidandtheweb@gmail.com>
2018-05-03 22:21:01 -0400

    Small syntax fixes/improvements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
-class UsersController < ApplicationController
-    def index
-    @users = User.all
-    end
+# frozen_string_literal: true
 
-    def show
+# :nodoc:
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
     @transaction = Transaction.new
     @user = User.find(params[:id])
     @lends = Transaction.where("lender_id = #{@user.id}")
     @borrows = Transaction.where("borrower_id = #{@user.id}")
-    end
+  end
 end
